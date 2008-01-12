e6941f9eeb608d5cbdd81c4b855fa2fcbb45bd72
Robert Olson <rko618@gmail.com>
2008-01-12 06:58:04 +0000

    Updated the gemspec to a rakefile. Renaming comes next.

--- gemspec.rb
+++ gemspec.rb
@@ -1,24 +1,44 @@
-require 'rubygems'
+require "rubygems"
+Gem::manage_gems
+require "rake/gempackagetask"
 
 spec = Gem::Specification::new do |s|
   s.name = "ruby-poker"
+  s.summary = "Ruby library for determining the winner in a game of poker." 
   s.version = "0.1.1" 
-  s.platform = Gem::Platform::RUBY
-  s.summary = "Ruby library for determining the winner in a game of poker."
-
-  cadidates = Dir.glob("{docs,examples,lib,test}/**/*")
-  s.files = candidates.delete_if do |item|
-    item.include?("svn")
-  end
   
+  s.rubyforge_project = "rubypoker"
+  s.platform = Gem::Platform::RUBY 
+  
+  s.autorequire = "rubypoker"
+  
+  s.files = FileList["lib/*.rb"]
   s.require_path = "lib" 
-  s.autorequire = "poker_hand"
 
   s.has_rdoc = File::exist? "docs" 
   s.extra_rdoc_files = ["README", "CHANGELOG", "LICENSE"]
-  s.test_file = "test/ts_rubypoker.rb" if File::directory? "test"
+  
+  s.test_files = Dir.glob("test/*.rb")
 
   s.author = "Robert Olson"
   s.email = "rko618@gmail.com"
   s.homepage = "http://rubyforge.org/projects/rubypoker/"
+end
+
+
+Rake::GemPackageTask.new(spec) do |pkg| 
+  pkg.need_tar = true 
+end
+
+task :default => "pkg/#{spec.name}-#{spec.version}.gem" do
+  putsCheck = `grep puts lib/*`
+  if putsCheck.size > 0
+   puts "********** WARNING: stray puts left in code"
+  end
+  puts "generated latest version"
+end
+
+task :docs do
+  `rm -rf doc`
+  `rdoc --line-numbers -U README CHANGELOG LICENSE lib/*.rb`
 end
