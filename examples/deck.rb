5696de96449794578cf60c79a6e381c8796862d1
Robert Olson <rko618@gmail.com>
2009-03-18 21:07:30 -0700

    Added methods #size and #burn to the sample Deck class as submitted by Kai de Haan

--- examples/deck.rb
+++ examples/deck.rb
@@ -19,8 +19,30 @@ class Deck
     shuffle()
   end
 
+  # removes a single card from the top of the deck and returns it
+  # synonymous to poping off a stack
   def deal
     @cards.pop
+  end
+  
+  # delete an array or a single card from the deck
+  # converts a string to a new card, if a string is given
+  def burn(burn_cards)
+    return false if burn_cards.is_a?(Integer)
+    if burn_cards.is_a?(Card) || burn_cards.is_a?(String)
+      burn_cards = [burn_cards]
+    end
+    
+    burn_cards.map! do |c|
+      c = Card.new(c) unless c.class == Card
+      @cards.delete(c)
+    end
+    true
+  end
+
+  # return count of the remaining cards
+  def size
+    @cards.size
   end
 
   def empty?
