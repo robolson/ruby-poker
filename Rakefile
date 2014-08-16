09f4da35f885a4262c26fc71d34498c8540d0575
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2014-08-16 19:28:08 +0200

    rearrange full house cards to return correct order when low pairs

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ Rake::TestTask.new do |t|
   t.libs = ['lib']
   t.verbose = true
   t.warning = true
-  t.test_files = FileList['test/test_card.rb', 'test/test_poker_hand.rb']
+  t.test_files = FileList['test/test_card.rb', 'test/test_poker_hand.rb', 'test/test_full_house.rb']
 end
 
 task :default => :test
---
+++ test/test_full_house.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper')
+
+class TestFullHouse < Test::Unit::TestCase
+
+  context "A Full House should return the correct cards from sort_using_rank" do
+
+    setup do
+      @fh_high_pair = PokerHand.new("8h 8d 8c Qh Td Kd Ks")
+      @fh_low_pair = PokerHand.new("8h 8d 8c Qh Td 4h 4d")
+      @fh_high_trips = PokerHand.new("Kh Kd Kc Qh Td 8h 8d")
+      @fh_low_trips = PokerHand.new("4h 4d 4c Qh Td Kd Ks")
+    end
+
+    should "full house #sort_using_rank from seven cards (high pair) should return correct cards" do
+      assert_equal("8h 8d 8c Ks Kd Qh Td", @fh_high_pair.sort_using_rank)
+    end
+
+    should "full house #sort_using_rank from seven cards (low pair) should return correct cards" do
+      assert_equal("8h 8d 8c 4h 4d Qh Td", @fh_low_pair.sort_using_rank)
+    end
+
+    should "full house #sort_using_rank from seven cards (high trips) should return correct cards" do
+      assert_equal("Kh Kd Kc 8h 8d Qh Td", @fh_high_trips.sort_using_rank)
+    end
+
+    should "full house #sort_using_rank from seven cards (low trips) should return correct cards" do
+      assert_equal("4h 4d 4c Ks Kd Qh Td", @fh_low_trips.sort_using_rank)
+    end
+
+  end
+
+end
